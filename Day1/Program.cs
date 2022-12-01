5002bd97a3916235c042116c3d705f75f528614d
KevinDitschke <kevin.ditschke@hotmail.de>
2022-12-01 19:56:31 +0100

    Part 1 successfully completed

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -4,6 +4,30 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine(GetHighestCaloryCount("inputTest.txt"));
+        Console.WriteLine(GetHighestCaloryCount("input.txt"));
+    }
+
+    private static int GetHighestCaloryCount(string fileName)
+    {
+        var file = File.ReadAllLines(fileName);
         
+        int temp = 0;
+        int sum = 0;
+        foreach (var line in file)
+        {
+            int output = 0;
+            if (int.TryParse(line, out output))
+                temp += output;
+            else
+            {
+                if (temp > sum)
+                {
+                    sum = temp;
+                }
+                temp = 0;
+            }
+        }
+        return sum;
     }
 }
