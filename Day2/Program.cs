a5ecaa7acb286eead57fb9cb54e3e5f88cb132cb
KevinDitschke <kevin.ditschke@hotmail.de>
2022-12-02 21:29:40 +0100

    Day 2 - Part 2 successfully completed

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -5,6 +5,57 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine(RockPaperScissorScore("input.txt"));
+        Console.WriteLine(RockPaperScissorScorePart2("input.txt"));
+    }
+
+    private static int RockPaperScissorScorePart2(string fileName)
+    {
+        var file = File.ReadAllLines(fileName);
+        var sum = 0;
+        foreach (var line in file)
+        {
+            if (line[2] == 'X')
+            {
+                sum += Lose(line[0]);
+            }
+            else if (line[2] == 'Y')
+            {
+                sum += Draw(line[0]) + 3;
+            }
+            else if (line[2] == 'Z')
+            {
+                sum += Win(line[0]) + 6;
+            }
+        }
+
+        return sum;
+    }
+
+    private static int Lose(char character)
+    {
+        var dict = new Dictionary<char, int>()
+        {
+            {'A', 3}, {'B', 1}, {'C', 2}
+        };
+        return dict[character];
+    }
+    
+    private static int Draw(char character)
+    {
+        var dict = new Dictionary<char, int>()
+        {
+            {'A', 1}, {'B', 2}, {'C', 3}
+        };
+        return dict[character];
+    }
+    
+    private static int Win(char character)
+    {
+        var dict = new Dictionary<char, int>()
+        {
+            {'A', 2}, {'B', 3}, {'C', 1}
+        };
+        return dict[character];
     }
 
     private static int RockPaperScissorScore(string fileName)
