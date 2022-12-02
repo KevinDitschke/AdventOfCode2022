c13cb3f8404d2b6e73f8e42979bd36a3e2580f09
KevinDitschke <kevin.ditschke@hotmail.de>
2022-12-02 17:50:42 +0100

    Day 2 - Part 1 successfully completed

---
+++ Day2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Day2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(RockPaperScissorScore("input.txt"));
+    }
+
+    private static int RockPaperScissorScore(string fileName)
+    {
+        var file = File.ReadAllLines(fileName);
+        var sum = 0;
+        foreach (var line in file)
+        {
+            switch (line)
+            {
+                case "A X":
+                    sum += 4;
+                    break;
+                case "A Y":
+                    sum += 8;
+                    break;
+                case "A Z":
+                    sum += 3;
+                    break;
+                case "B X":
+                    sum += 1;
+                    break;
+                case "B Y":
+                    sum += 5;
+                    break;
+                case "B Z":
+                    sum += 9;
+                    break;
+                case "C X":
+                    sum += 7;
+                    break;
+                case "C Y":
+                    sum += 2;
+                    break;
+                case "C Z":
+                    sum += 6;
+                    break;
+            }
+        }
+
+        return sum;
+    }
+}
