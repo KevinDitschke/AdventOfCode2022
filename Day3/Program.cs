8459adbdae744932815a58949b298016de5ac96e
KevinDitschke <kevin.ditschke@hotmail.de>
2022-12-03 14:17:32 +0100

    Day 3 - Part 2 successfully completed

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -2,33 +2,39 @@
 
 internal class Program
 {
-    private static Dictionary<char, int> priorities; 
+    private static Dictionary<char, int> priorities;
 
     static void Main(string[] args)
     {
         priorities = new Dictionary<char, int>();
         for (int i = 1; i < 27; i++)
         {
-            priorities.Add((char)(i+64), i+26);
-            priorities.Add((char)(i+96), i);
+            priorities.Add((char)(i + 64), i + 26);
+            priorities.Add((char)(i + 96), i);
         }
-        Console.WriteLine(MostCommonRucksackItem("inputTest.txt"));
-        Console.WriteLine(MostCommonRucksackItem("input.txt"));
+
+        // Console.WriteLine(GroupsOfThree("inputTest.txt"));
+        Console.WriteLine(GroupsOfThree("input.txt"));
     }
 
-    private static int MostCommonRucksackItem(string fileName)
+    private static int GroupsOfThree(string fileName)
     {
         var file = File.ReadAllLines(fileName);
+        var groups = file.Chunk(3);
+        var totalsum = 0;
+        foreach (var rucksacks in groups)
+        {
+            totalsum += MostCommonRucksackItem(rucksacks);
+        }
+        return totalsum;
+    }
+
+    private static int MostCommonRucksackItem(string[] rucksacks)
+    {
         var sum = 0;
 
-        foreach (var rucksack in file)
-        {
-            var firstCompartment = rucksack.Substring(0, rucksack.Length/2) ;
-            var secondCompartment = rucksack.Substring(rucksack.Length/2, rucksack.Length/2);
-
-            var duplicate = firstCompartment.First(x => secondCompartment.Contains(x));
-            sum += priorities[duplicate];
-        }
+        var duplicate = rucksacks[0].First(x => rucksacks[1].Contains(x) && rucksacks[2].Contains(x));
+        sum += priorities[duplicate];
         return sum;
     }
 }
