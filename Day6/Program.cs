f96a04ffc7eeb61dafc1a69ab46cb16202e5bd6d
k.ditschke <l.ditschke@schenckprocess.com>
2022-12-06 09:41:31 +0100

    Day 6 - Part 1 successfully completed

---
+++ Day6/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Day5;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(ReadingBuffer("inputTest.txt"));
+        Console.WriteLine(ReadingBuffer("input.txt"));
+    }
+
+    private static int ReadingBuffer(string fileName)
+    {
+        var text = ReadFile(fileName);
+        var position = 0;
+        for (var i = 0; i < text.Length-4; i++)
+        {
+            var subString = text.Substring(i, 4);
+            if (subString.Select(x => x).Distinct().Count() == 4){
+                position = i + 4;
+                break;
+            }
+        }
+        return position;
+    }
+
+    private static string ReadFile(string fileName)
+    {
+        return File.ReadAllText(fileName);
+    }
+}
